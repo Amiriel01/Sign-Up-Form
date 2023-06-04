624af8a30d1df42b9430666043650f454c9d96fd
Sarah Quinney <quinney.sarah@gmail.com>
2023-06-04 12:34:32 -0500

    js in functional added second image for readme

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function passwordConfirmation() {
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("password2").value;
-   
-    if (password === "") {
-        alert("Error: The password field is Empty.");
-    } else if (password === confirmPassword) {
-        alert("Account Created");
+
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("password2").value;
+
+    if (password == "") {
+        document.getElementById("message").innerHTML = "**Error: The password field is Empty.";
+    } else if (password == confirmPassword) {
+        document.getElementById("message").innerHTML = "**Account Created";
+        //return true; (When I want it to actually work)//
     } else {
-        alert("Please make sure your passwords match.")
+        document.getElementById("message").innerHTML = "**Please make sure your passwords match.";
     }
+    return false;
 }
+
