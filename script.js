c454eea8490854bd1b9c521e419c335286dea03e
Sarah Quinney <quinney.sarah@gmail.com>
2023-06-04 12:52:09 -0500

    updated readme images, and js

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ function passwordConfirmation() {
 
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("password2").value;
-
-    if (password == "") {
-        document.getElementById("message").innerHTML = "**Error: The password field is Empty.";
-    } else if (password == confirmPassword) {
+    
+    if (password == confirmPassword) {
         document.getElementById("message").innerHTML = "**Success: Account Created!";
         //return true; (When I want it to actually submit the form on a website)//
     } else {
-        document.getElementById("message").innerHTML = "**Error: Please make sure your passwords match.";
+        document.getElementById("message").innerHTML = "**Error: Passwords do not match.";
     }
     return false;
-}
 
+}
